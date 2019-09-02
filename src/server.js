a1245147a579fb737eca245beb1aeaa28887ee54
Serhii_Y <dj.ser961@gmail.com>
2019-09-02 23:39:24 +0300

    Moved db folders to src, deleted db folder

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const url = require('url');
 
-const router = require('./db/router');
+const router = require('./router');
 const {idFreeUrl, id, param} = require('./helpers/url-id');
 
 const startServer = port => {
@@ -11,20 +11,6 @@ const startServer = port => {
     // Get router function using url without id and id as a argument
     (idFreeUrl(router, parsedUrl.pathname, params) || router.default)(req, res, id(parsedUrl.pathname, params), param(params));
   });
-
-  // server.on('request', (req, res) => {
-    
-
-  //   if(req.method === 'GET'){
-  //     if(req.url === '/products/:id') {
-  //     } else
-
-  //     { 
-  //       res.writeHead(404, {"Content-type": "application/json"});
-  //       res.end('Route is not found!');
-  //     };
-  //   } //GET
-  // }); //server.on
 
   server.listen(port, (err) => {
     if (err) return console.log('Something is going wrong:', err);
